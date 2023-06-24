e0af510c98df0fdaa0e21d96a999131bd3f070a4
Phahladira-Moja <phahladira@patala.co.za>
2023-06-24 21:48:08 +0200

    Created and Finished Map component

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,14 +1,23 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Map from "../components/Map";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const MapScreen = () => {
+  const Stack = createStackNavigator();
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>MapScreen</Text>
+    <View>
+      <View className="h-1/2">
+        <Map />
       </View>
-    </SafeAreaView>
+      <View className="h-1/2">
+        <Stack.Navigator>
+          <Stack.Screen></Stack.Screen>
+        </Stack.Navigator>
+      </View>
+    </View>
   );
 };
 
